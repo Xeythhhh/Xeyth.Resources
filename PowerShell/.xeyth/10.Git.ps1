9f96cd9ceb884f82028f6ca5daaff1954ac1ab38
Xeyth <30859995+Xeythhhh@users.noreply.github.com>
2025-04-16 05:39:54 +0300

    refactor(powershell): consolidate Git editor detection
    - replaced inline logic with a GitEditor class and structured return type
    - improved clarity and separation of concers between detection and configuration
    - simplified usage and reduced code duplication

--- PowerShell/.xeyth/10.Git.ps1
+++ PowerShell/.xeyth/10.Git.ps1
@@ -1,14 +1,35 @@
 & {
-    function Set-GitEditorBasedOnEnvironment {
-        switch -Regex ((Get-Process -Id $PID).Parent.ProcessName) {
-            { $_ -match "Code" }            { git config --global core.editor "code --wait";   return "Git editor set to: VS Code" }
-            { $_ -match "Cursor" }          { git config --global core.editor "cursor --wait"; return "Git editor set to: Cursor" }
-            { $_ -match "rider64" }         { git config --global core.editor "nano";          return "Git editor set to: nano (via Rider)" }
-            { $_ -match "ServiceHub.Host" } { git config --global core.editor "nano";          return "Git editor set to: nano (via Visual Studio)" }
-            default                         { git config --global core.editor "nano";          return "Git editor set to: nano (default)" }
-        }
-        
-    }
+	class GitEditor {
+		[ValidateNotNullOrEmpty()][string]$Tool
+		[ValidateNotNullOrEmpty()][string]$Info
+	
+		GitEditor([string]$tool, [string]$info) {
+			$this.Tool = $tool
+			$this.Info = $info
+		}
+	}
+	
+	function Get-GitEditorBasedOnEnvironment([string]$parent) {
+		$tool = ""; $info = ""
+	
+		switch -Regex ($parent) {
+			{ $_ -match "Code" }            { $tool = "code --wait"   ; $info = "VS Code" }
+			{ $_ -match "Cursor" }          { $tool = "cursor --wait" ; $info = "Cursor" }
+			{ $_ -match "rider64" }         { $tool = "nano"          ; $info = "nano (via Rider)" }
+			{ $_ -match "ServiceHub.Host" } { $tool = "nano"          ; $info = "nano (via Visual Studio)" }
+			default                         { $tool = "nano"          ; $info = "nano (default)" }
+		}
+	
+		return [GitEditor]::new($tool, $info)
+	}
+    
+	function Set-GitEditorBasedOnEnvironment {
+		$parent = (Get-Process -Id $PID).Parent.ProcessName
+		$editor = Get-GitEditorBasedOnEnvironment $parent
+		
+		git config --global core.editor $editor.Tool
+		return $editor.Info
+	}
 
     ######################################################################
 
